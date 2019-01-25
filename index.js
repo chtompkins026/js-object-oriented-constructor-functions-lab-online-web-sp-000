2e0d1d1322888317d5d80eae27180011963a8b74
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-25 18:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function Scooter(year, color, model){
 
 }
 
-function Driver(name, age, model){ 
-  this.year = year; 
-  this.color = color; 
-  this.model = model; 
+function Driver(name, age, experience){ 
+  this.name = name; 
+  this.age = age; 
+  this.experience = experience; 
 
 }
